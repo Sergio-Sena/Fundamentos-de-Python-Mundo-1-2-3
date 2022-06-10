2a5696351a45a706e17267e772b670f01ab67862
sergio-programador <senanetworker@gmail.com>
2022-06-10 20:58:56 -0300

    Desafio002.py

---
+++ Desafio002.py
@@ -0,0 +1,5 @@
+#---------------------------------------#
+print('Antecessor e sucessor')
+#---------------------------------------#
+n = int(input('Digite um número :'))
+print('O antecessor de {} é {} e o sucessor é {}'.format(n,n-1,n+1))
