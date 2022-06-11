93c7595de6d2847dfb09469683797c11cbf5e4e3
sergio-programador <senanetworker@gmail.com>
2022-06-11 17:01:54 -0300

    up date Python

---
+++ Desafio008.py
@@ -0,0 +1,4 @@
+# conversor de medidas#
+
+m = float(input('Entre com a metragem a converter :'))
+print('{} metros convertidos em centimetos vale {} centimetros e convertidos em mm vale {} milímetros '.format(m,100/m,1000/m))
