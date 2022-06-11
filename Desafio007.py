8923f59ddde1b6ede4da03292bc93d31183b9a10
sergio-programador <senanetworker@gmail.com>
2022-06-11 13:08:20 -0300

    Update code

---
+++ Desafio007.py
@@ -0,0 +1,6 @@
+# Media de aluno #
+
+nome = input('Digite o nome do aluno :')
+n1 = float(input('Digite a primeira nota :'))
+n2 = float(input('Digite a segunda nota :'))
+print('A média do aluno {} foi {:.1f}'.format(nome, (n1+n2)/2))
