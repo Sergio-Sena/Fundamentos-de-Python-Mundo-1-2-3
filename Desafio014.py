bf595397145b5ff29d996fb01955ead94a58698f
sergio-programador <senanetworker@gmail.com>
2022-06-13 14:51:21 -0300

    update

--- Desafio014.py
+++ Desafio014.py
@@ -1,3 +1,3 @@
 # Conversor de temperatura
 temperatura = float(input('Digite a temperatura C°:'))
-print('A temperatura {:.0f} C° corresponde a {:.0f} F°'.format(temperatura,temperatura+33.8))
+print('A temperatura {:.0f} C° corresponde a {:.0f} F°'.format(temperatura,(temperatura*9/5+32)))
