bc026995e0b72fe78aed8aaa955eda0f8f05f319
sergio-programador <senanetworker@gmail.com>
2022-06-13 13:16:56 -0300

    up date

--- Desafio011.py
+++ Desafio011.py
@@ -3,4 +3,4 @@
 alt = float(input('Entre com a altura :'))
 lar = float(input('Entre com a largura :'))
 a = (alt*lar)
-print('Para pintar uma área de {} metros quadrados serão necessários {} litros de tinta.'.format(a,a/2))
+print('Sua parede de {} x {} tem uma área de {} m² e precisará de {:.2f} litros de tinta para pinta-la'.format(alt,lar,a,a/2))
--- Desafio012.py
+++ Desafio012.py
@@ -1,3 +1,3 @@
 # Desconto de 5% # 
-v = float(input('Digite o valor do produto :'))
+v = float(input('Digite o valor do produto R$:'))
 print('O valor do produto com desconto é de {:.2f}'.format(v-(v*0.05)))
