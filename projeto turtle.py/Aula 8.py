b8880e0cdcb23aa62c73dd34e4107414c5a91689
sergio-programador <senanetworker@gmail.com>
2022-06-14 16:36:44 -0300

    update

--- projeto turtle.py/Aula 8.py
+++ projeto turtle.py/Aula 8.py
@@ -14,23 +14,3 @@ import random
 num = random.randint(1, 10)
 print(num)
 #===================#
-
-import turtle
-ninja = turtle.Turtle()
-ninja.speed(500)
-
-for i in range (180):
-    ninja.forward(100)
-    ninja.right(30)
-    ninja.forward(20)
-    ninja.left(60)
-    ninja.forward(50)
-    ninja.forward(30)
-
-    ninja.penup()
-    ninja.setposition(0 , 0)
-    ninja.pendown()
-
-    ninja.right(2)
-
-turtle.done()
