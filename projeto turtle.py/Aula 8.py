98a4f8f4d95de129775d7505050e5f9492860694
sergio-programador <senanetworker@gmail.com>
2022-06-14 09:39:18 -0300

    Turtle projeto e aula 8

--- projeto turtle.py/Aula 8.py
+++ projeto turtle.py/Aula 8.py
@@ -3,6 +3,8 @@
 # from modulo item
 
 import math
+
+import emoji
 num = int(input('Digte um número :'))
 raiz = math.sqrt(num)
 print('A raiz quadrada de {} aredondada para cima é igual a {}'.format(num,math.ceil(raiz)))
@@ -12,3 +14,23 @@ import random
 num = random.randint(1, 10)
 print(num)
 #===================#
+
+import turtle
+ninja = turtle.Turtle()
+ninja.speed(500)
+
+for i in range (180):
+    ninja.forward(100)
+    ninja.right(30)
+    ninja.forward(20)
+    ninja.left(60)
+    ninja.forward(50)
+    ninja.forward(30)
+
+    ninja.penup()
+    ninja.setposition(0 , 0)
+    ninja.pendown()
+
+    ninja.right(2)
+
+turtle.done()
