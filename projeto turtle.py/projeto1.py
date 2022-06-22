8dae81e3e6c1b963874c62d23e2ecd5116c21359
sergio-programador <senanetworker@gmail.com>
2022-06-22 19:06:12 -0300

    update turtle

--- projeto turtle.py/projeto1.py
+++ projeto turtle.py/projeto1.py
@@ -8,15 +8,16 @@ for i in range (90):
     ninja.forward(100)
     ninja.right(30)
     ninja.forward(20)
-    ninja.forward(50)
     ninja.left(60)
+    ninja.forward(50)
     ninja.forward(30)
+    ninja.right(2)
 
     ninja.penup()
     ninja.setposition(0 , 0)
     ninja.pendown()
 
-    ninja.right(2)
+    
 
 turtle.done()
 
