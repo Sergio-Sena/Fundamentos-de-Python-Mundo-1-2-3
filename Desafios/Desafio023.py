1d7d22512930b2d6bdea42c735650e677c195da7
Sergio-Sena <senanetworker@gmail.com>
2022-07-04 18:32:16 -0300

    update

--- Desafios/Desafio023.py
+++ Desafios/Desafio023.py
@@ -5,10 +5,10 @@ print('''#Desafio#
 # 3-quantas letras ao todo sem considerar espaços
 # 4-quantas letras tem o primeiro nome #''')
 
-nome = input('Digite seu nome completo: ')
+nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome.....')
 print('Seu nome em maiúsculas:', (nome.upper()))
 print('Seu nome em maiúsculas:', (nome.lower()))
-print('Letras ao todo sem considerar espaços:', (len(nome)))
+print('Letras ao todo sem considerar espaços:', (len(nome)-nome.count(' ')))
 pn = nome.split()
 print('Seu Primeiro nome é :',pn[0],'e contém',(len(pn[0])),'letras.')
