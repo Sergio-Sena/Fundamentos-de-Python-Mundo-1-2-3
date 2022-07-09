7d96f567acf9bbe7127f6cbf93a8ac5bd9dd45a5
Sergio-Sena <senanetworker@gmail.com>
2022-07-09 10:17:06 -0300

    Add cores

--- Desafios/Desafio007.py
+++ Desafios/Desafio007.py
@@ -1,6 +1,10 @@
 # Media de aluno #
+cores = {'limpa':'\033[m',
+'azul':'\033[34m',
+'ciano':'\033[36m',
+'media':'0;33;45'}
 
 nome = input('Digite o nome do aluno :')
 n1 = float(input('Digite a primeira nota :'))
 n2 = float(input('Digite a segunda nota :'))
-print('A média do aluno {} foi {:.1f}'.format(nome, (n1+n2)/2))
+print('A média do aluno {}{}{} foi {}{:.1f}'.format(cores['ciano'],nome,cores['limpa'], (n1+n2)/2))
