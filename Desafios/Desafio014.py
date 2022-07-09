f4af77ead6d73d71aa3b9828ff82524adcfe882c
Sergio-Sena <senanetworker@gmail.com>
2022-07-09 14:09:56 -0300

    Add Alteração do nome

--- Desafios/Desafio014.py
+++ Desafios/Desafio014.py
@@ -1,3 +1,6 @@
 # Conversor de temperatura
+from re import M
+
+
 temperatura = float(input('Digite a temperatura C°:'))
 print('A temperatura {:.0f} C° corresponde a {:.0f} F°'.format(temperatura,(temperatura*9/5+32)))
