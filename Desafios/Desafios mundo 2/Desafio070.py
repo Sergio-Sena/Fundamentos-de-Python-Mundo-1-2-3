9da8625f3bb48878d967505d5b3680a760e2bffb
Sergio-Sena <senanetworker@gmail.com>
2022-08-03 14:39:56 -0300

    Add caixa eletronico

--- Desafios/Desafios mundo 2/Desafio070.py
+++ Desafios/Desafios mundo 2/Desafio070.py
@@ -1,17 +1,23 @@
-#Exercício Python 70: Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre:
+total = totmil = menor = cont = 0
+barato = ' '
+while True:
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Digite o preço R$: '))
+    cont += 1
+    total += preço
+    if preço > 1000:
+        totmil += 1
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar?[S/N]: ')).strip().upper()[0]
+    if resp == 'N':
+        break
 
-# A) qual é o total gasto na compra.
+print('{:-^40}'.format('Fim do programa'))
+print(f'O total da sua compra foi {total:.2f}.')
+print(f'Temos {totmil} produtos acima de R$ 1000')
+print(f'O produto mais barato foi {barato} que custa R$ {menor:.2f}')
 
-# B) quantos produtos custam mais de R$1000.
-
-# C) qual é o nome do produto mais barato.
-
-
-print('+-'*25)
-print(' Utencílios Sergio Sena ')
-print('+-'*25)
-
-item = str(input('Digite o produto: '))
-valor = float(input('Digite o valor do produto: '))
-resp = ('Quer continuar? [S/N]: ').upper().strip()[0]
-print(f'Item:{item} Valor:{valor} Continuar?{resp}')    
