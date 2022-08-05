126ae7fc407008aba6d60aa35decc5048823909f
Sergio-Sena <senanetworker@gmail.com>
2022-08-05 14:06:55 -0300

    Update desafio 073

---
+++ Desafios/Desafios mundo 3/Desafio073.py
@@ -0,0 +1,25 @@
+#Exercício Python 73: Crie uma tupla preenchida com os 20 primeiros colocados da Tabela do Campeonato Brasileiro de Futebol, na ordem de colocação. Depois mostre:
+
+# a) Os 5 primeiros times.
+
+# b) Os últimos 4 colocados.
+
+# c) Times em ordem alfabética.
+
+# d) Em que posição está o time da Chapecoense.
+
+lista = ( 'palmeiras','corinthians', 'Fluminense','Atlético','flamengo','Internacional','AtleticoMG','Bragantino','santos','São Paulo','Goias', 'Borafogo','AmericaMG','Ceará','Coritiba','Avai','Cuiaba','Fortaleza', 'AtleticoGo','juventude.')
+print('-='*20)
+print('{:-^30}'.format('Campeonato Brasileiro.'))
+print('-='*20)
+print('-='*30)
+print(f'Os 5 primeiros colocados são {lista[0:6]}')
+print(f'Os 4 Últimos colocados são {lista[-4:]}')
+print('-='*30)
+print('-='*30)
+print(f'Times em ordem Alfabética {sorted(lista)}')
+print('-='*30)
+if lista[0:] =='chapeco':
+    print(lista['chapeco'])
+else:
+    print('Chapecó não esta classificado.')
