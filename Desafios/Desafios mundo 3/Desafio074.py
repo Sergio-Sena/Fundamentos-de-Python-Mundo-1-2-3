76f6b738944df4b2bdbdc6beca87bd1e443adb2c
Sergio-Sena <senanetworker@gmail.com>
2022-08-05 17:07:47 -0300

    add desafio074 primeiros códigos

---
+++ Desafios/Desafios mundo 3/Desafio074.py
@@ -0,0 +1,7 @@
+# Exercício Python 074: Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla. Depois disso, mostre a listagem de números gerados e também indique o menor e o maior valor que estão na tupla.
+from random import randint
+
+for lista in range(0,5):
+    lista = (randint(0,10))
+    print(lista)
+
