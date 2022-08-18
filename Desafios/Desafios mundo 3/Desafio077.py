47527c85cd92e022208941e4beebee8afaafac8a
Sergio-Sena <senanetworker@gmail.com>
2022-08-18 16:40:26 -0300

    add desafio 077

--- Desafios/Desafios mundo 3/Desafio077.py
+++ Desafios/Desafios mundo 3/Desafio077.py
@@ -1,8 +1,7 @@
 # Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-resp = ''
-while True:
-    palavras = str(input('Dgite as palavras: '))
-    resp = str(input('Mais palavras? [S/N]')).upper().strip()
-    if resp == 'N':
-        break
-    print(palavras)
+palavras = ('aprender','python','curso','mercado','trabalhar','programador','estudar')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end ='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end = '')
