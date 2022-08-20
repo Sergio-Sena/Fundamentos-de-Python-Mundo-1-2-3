8d938c00cfc69e8c47e64194741ae63907a73119
Sergio-Sena <senanetworker@gmail.com>
2022-08-20 16:40:58 -0300

    Add desafio 080 solução

--- Desafios/Desafios mundo 3/Desafio080.py
+++ Desafios/Desafios mundo 3/Desafio080.py
@@ -1,11 +1,19 @@
 #Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.
 
-from tkinter import INSERT
+lista = []
+for c in range(0, 5):
+        n = int(input('Digite o valor: '))
+        if c == 0 or n > lista[-1]:
+            lista.append(n)
+            print('Adicionado no final da lista.')
+        else:
+            pos = 0
+            while pos < len(lista):
+                if n <= lista[pos]:
+                    lista.insert(pos, n)
+                    print(f'Adicionado na posição {pos} da lista.')
+                    break
+                pos += 1        
+print('-='*30)                
+print(f'Os valores digitados em ordem  foram: {lista}')
 
-
-n = list()
-for c in range(0,5):
-    n = 0
-    if n == 0:
-        n.append(int(input('Digite um valor: ')))
-print(n)
