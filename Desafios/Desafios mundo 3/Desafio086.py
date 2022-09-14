d36faac6a4e44459eb94cef17b526a2a147c7b54
Sergio-Sena <senanetworker@gmail.com>
2022-09-14 16:09:14 -0300

    update

---
+++ Desafios/Desafios mundo 3/Desafio086.py
@@ -0,0 +1,1 @@
+# Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3×3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
