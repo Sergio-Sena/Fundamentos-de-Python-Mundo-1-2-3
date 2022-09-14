d573bc542e37e9a6014e49e96a3d5569f695f813
Sergio-Sena <senanetworker@gmail.com>
2022-09-14 15:25:27 -0300

    Up dade Desafio084

--- Desafios/Desafios mundo 3/Desafio084.py
+++ Desafios/Desafios mundo 3/Desafio084.py
@@ -4,12 +4,14 @@
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
 
-# Ler nome e peso
-from time import sleep
 
+# importar lib de tempo
+from time import sleep
+# Ler nome e peso
 temp = []
 princ = []
 mai = men = 0
+# Alimentar e validar dados
 while True:
     temp.append(str(input("Digite o nome: ")))
     temp.append(float(input("Digite o peso: ")))
@@ -23,15 +25,19 @@ while True:
     princ.append(temp[:])
     temp.clear()
     resp = str(input("Quer contnuar?[S/N]: "))
+# Quebra de Loop
     if resp in "Nn":
         break
 print("-=" *30)
+# Impressão de dados
 print(f"Você cadastrou {len(princ)} pessoas")
+# Validação de maoir peso
 print(f"O maior peso foi {mai} kg. Peso de",end ='')
 for p in princ:
     if p[1] == mai:
         print(f"{[p[0]]}")
 print('-=' *30)
+# Validação de menor peso
 print(f'O menor peso foi {men} kg. Peso de ',end ='')
 for p in princ:
     if p[1] == men:
