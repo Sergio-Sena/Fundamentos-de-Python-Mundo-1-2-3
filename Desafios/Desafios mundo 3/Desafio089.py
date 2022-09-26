64000ab7554b18b7c126c11418ff15a1bc83de15
Sergio-Sena <senanetworker@gmail.com>
2022-09-26 16:12:55 -0300

    Add desafio 089 update

--- Desafios/Desafios mundo 3/Desafio089.py
+++ Desafios/Desafios mundo 3/Desafio089.py
@@ -1,7 +1,29 @@
 # Exercício Python 089: Crie um programa que leia nome e duas notas de vários alunos e guarde tudo em uma lista composta. No final, mostre um boletim contendo a média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
-boletin = [[],[],[],[]]
-media = []
-boletin[0].append(str(input("Digite o nome do Aluno: ")))
-boletin[1].append(float(input("Digite a 1° nota: ")))
-boletin[2].append(float(input("Digite a 2° nota: ")))
-print(f'O Aluno {boletin[0]} teve as notas {boletin[1]} e {boletin[2]}')
+from time import sleep
+
+ficha = list()
+while True:
+    nome = str(input('Digite o nome do aluno: '))
+    n1 = float(input('Digite a Nota 1: '))
+    n2 = float(input('Digite a Nota 2: '))
+    media = (n1 + n2)/2
+    ficha.append([nome, [n1, n2], media])
+    resp = str(input('Quer continuar? [S/N] )'))
+    if resp in "Nn":
+        break
+print('-='*30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print("-" * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('-' * 35)
+    opc = int(input('Mostrar a nota de qual aluno? (999 interrompe): ' ))   
+    if opc == 999:
+        print('Finalizando....')
+        sleep(2)
+        break
+    if opc <= len(ficha)-1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('-=' *30)
+print('<<<Volte sempre>>>')
