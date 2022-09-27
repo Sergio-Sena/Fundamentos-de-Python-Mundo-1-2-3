05db6ebff15670cc740176e246e6453fe00b1c55
Sergio-Sena <senanetworker@gmail.com>
2022-09-27 12:36:23 -0300

    desafio 090 solução

--- Desafios/Desafios mundo 3/Desafio090.py
+++ Desafios/Desafios mundo 3/Desafio090.py
@@ -1,1 +1,12 @@
 # Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+aluno = dict()
+aluno['nome'] = str(input("Nome do aluno: "))
+aluno['média'] = float(input(f'Média do aluno {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['Situação'] = 'Aprovado.'
+elif 5 <= aluno['média'] < 7:
+    aluno['Situação'] = 'Recuperação.'
+else:
+    aluno['Situação'] = 'Reprovado.'
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
