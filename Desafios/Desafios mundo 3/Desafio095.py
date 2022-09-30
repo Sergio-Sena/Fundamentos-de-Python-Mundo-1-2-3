3eb368141e54f68a2b3243f6a1ef4b4902cc92ff
Sergio-Sena <senanetworker@gmail.com>
2022-09-30 13:51:52 -0300

    add desafio 095

--- Desafios/Desafios mundo 3/Desafio095.py
+++ Desafios/Desafios mundo 3/Desafio095.py
@@ -1,1 +1,46 @@
 # Exercício Python 095: Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
+time = list()
+jogador = {}
+partidas = list()
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["nome"]} jogou?: '))
+    partidas.clear()
+    for c in range(0,tot):
+        partidas.append(int(input(f'Quantos gols na partida {c+1}?: ')))
+    jogador['gols'] = partidas[:] 
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input("Quer continuar[S/N]?:  ")).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-='*30)
+print('cod ',end = '')
+for i in jogador.keys():
+    print(f'{i:<15}', end = '')
+print('')
+print('-='*40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ',end = '')
+    for d in v.values():
+        print(f'{str(d):<15}',end = '')
+print('')
+print('-='*40)
+while True:
+    busca = int(input('Mostrar os dados de qual jogador? (999 para parar)'))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'ERRO! Não existe jogador com o COD {busca}!.')
+    else:
+        print(f' -- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}: ')
+        for i,g in enumerate(time[busca]['gols']):
+            print(f'    No jogo {i+1} fez {g} gols.')
+print('')
+print('<<<Volte Sempre>>>')
