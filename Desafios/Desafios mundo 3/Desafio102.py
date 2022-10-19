d55e7636732df67bd4dd630a8e92cf9d3d5f7341
sergio-sena <senanetworker@gmail.com>
2022-10-19 20:12:01 -0300

    update de desafio 102

--- Desafios/Desafios mundo 3/Desafio102.py
+++ Desafios/Desafios mundo 3/Desafio102.py
@@ -1,4 +1,22 @@
 # Exercício Python 102: Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
+def fatorial (n, show=False):
+    """
+    -> Calcula o fatorial do numero informado
+    : paran n:O numero a ser calculado
+    : paran show: (Opcional) mostra ou nao a conta
+    : return : retorna o valor de n.
+    """
+    f =1
+    for c in range(n,0,-1):
+        if show:
+            print (c,end='')
+            if c > 1:
+                print('x ',end='')
+            else:
+                print('= ',end='')
+        f *= c
+    return f
 
-
-
+#Programa principal
+print(fatorial(5,show=False))    
+help(fatorial)
