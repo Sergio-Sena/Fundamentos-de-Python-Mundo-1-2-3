c2e9698bfe9f7e3fb66efd0716d0dada047b6262
sergio-sena <senanetworker@gmail.com>
2022-11-08 17:47:53 -0300

    Update TXT

--- Desafios/Desafios mundo 3/Desafio105.py
+++ Desafios/Desafios mundo 3/Desafio105.py
@@ -1,5 +1,4 @@
 # Exercício Python 105: Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
-
 # – Quantidade de notas
 # – A maior nota
 # – A menor nota
